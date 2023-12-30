babf81895c4814b5ef6f8e58b1ab59057f89f183
SANDROJV <74668595+SANDROJV@users.noreply.github.com>
2023-12-31 01:50:03 +0400

    Validation and CSS added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,38 +6,47 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-project';
 
-  constructor(private authService: AuthService, private adminService: AdminService) {
-    fetch("http://lukabudagovi-001-site1.atempurl.com/api/User/users")
-      .then(response => response.json())
-      .then(data => console.log(JSON.stringify(data)));
+  constructor(
+    private authService: AuthService,
+    private adminService: AdminService
+  ) {
+    fetch('http://lukabudagovi-001-site1.atempurl.com/api/User/users')
+      .then((response) => response.json())
+      .then((data) => console.log(JSON.stringify(data)));
 
     // fetch("http://lukabudagovi-001-site1.atempurl.com/api/User/jobs")
     //   .then(response => response.json())
     //   .then(data => console.log(JSON.stringify(data)));
-      
-    //adminService.deleteUser(45);
+
+    // fetch("http://lukabudagovi-001-site1.atempurl.com/api/Admin/delete-user/89", {
+    //   method: "delete"
+    // })
   }
 
   addJob() {
-    this.adminService.addJob("cook").subscribe((data) => {
-      console.log(data)
-    },
-    err => {
-      console.error(err)
-    });
+    this.adminService.addJob('cook').subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
   }
 
   deleteUser() {
-    this.adminService.deleteUser(81).subscribe((data) => {
-      console.log(data)
-    },
-    err => {
-      console.error(err)
-    });
+    this.adminService.deleteUser(81).subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
   }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -9,14 +14,15 @@ import { UserService } from 'src/app/services/user.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule]
+  imports: [ReactiveFormsModule, CommonModule],
 })
-
 export class RegisterComponent {
-
   jobs: any[] = [];
 
-  constructor(private userService: UserService, private authService: AuthService) {
+  constructor(
+    private userService: UserService,
+    private authService: AuthService
+  ) {
     userService.getJobs().subscribe(
       (data) => {
         const arr = Object.values(data);
@@ -29,35 +35,81 @@ export class RegisterComponent {
   }
 
   registerForm = new FormGroup({
-    firstname: new FormControl('', { nonNullable: true, validators: Validators.required }),
-    lastname: new FormControl('', { nonNullable: true, validators: Validators.required }),
-    email: new FormControl('', { nonNullable: true, validators: Validators.required }),
-    password: new FormControl('', { nonNullable: true, validators: Validators.required }),
-    confirmPass: new FormControl('', { nonNullable: true, validators: Validators.required }),
-    jobId: new FormControl(0, { nonNullable: true, validators: Validators.required })
-  })
+    firstname: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    lastname: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    confirmPass: new FormControl('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+    jobId: new FormControl(null, {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+  });
 
   get form() {
     return this.registerForm.controls;
   }
 
-  onSubmit() {
+  checkPassword() {
+    const passwordControl = this.registerForm.get('password');
+    const confirmPassControl = this.registerForm.get('confirmPass');
 
+    if (passwordControl && confirmPassControl) {
+      const passwordValue = passwordControl.value;
+      const confirmPassValue = confirmPassControl.value;
+
+      if (passwordValue.length >= 6) {
+        if (passwordValue === confirmPassValue) {
+          passwordControl.setErrors(null);
+          confirmPassControl.setErrors(null);
+          return true;
+        } else {
+          passwordControl.setErrors({ mismatch: true });
+          confirmPassControl.setErrors({ mismatch: true });
+          return false;
+        }
+      } else {
+        alert('Password does not meet the requirements or is invalid.');
+        return false;
+      }
+    }
+
+    return false;
+  }
+
+  onSubmit() {
     const firstname = this.registerForm.get('firstname')?.value;
     const lastname = this.registerForm.get('lastname')?.value;
     const email = this.registerForm.get('email')?.value;
     const password = this.registerForm.get('password')?.value;
-    const jobId = +this.registerForm.get('jobId')!.value;
+    const jobId = this.registerForm.get('jobId')?.value as number | null;
 
-    console.log(password);
-    this.authService.register(firstname, lastname, email, password, jobId)
-      .subscribe(
-        data => {
-          console.log(data);
-        },
-        err => {
-          console.error(err.message);
-        }
-      );
+    if (this.checkPassword()) {
+      this.authService
+        .register(firstname, lastname, email, password, jobId)
+        .subscribe(
+          (data) => {
+            console.log(data);
+          },
+          (err) => {
+            console.error(err.message);
+          }
+        );
+    }
   }
 }
