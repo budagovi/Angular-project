d2f777159bf6aeef7ef1441c8d96ed6a762390f4
user <you@example.com>
2024-01-07 03:40:32 +0400

    background

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
-import { UserService } from '../../services/user.service';
 import { TokenService } from '../../services/token.service';
 import { Router, RouterLink } from '@angular/router';
 import { ModalComponent } from '../modal/modal.component';
